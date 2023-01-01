ed8a0cf2d9c169edca2ceae445a887f30fdde814
zengrc <1756332466@qq.com>
2023-01-01 19:56:06 +0800

    [Deprecated Shiro] deprecated SecureRandomNumberGenerator

--- src/main/java/com/ray/dormitory/util/Md5Util.java
+++ src/main/java/com/ray/dormitory/util/Md5Util.java
@@ -1,7 +1,8 @@
 package com.ray.dormitory.util;
 
-import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
 
 /**
  * md5加密和获取加密盐
@@ -16,7 +17,7 @@ public class Md5Util {
     }
 
     public static String getSalt() {
-        return new SecureRandomNumberGenerator().nextBytes().toString();
+        return UUID.randomUUID().toString();
     }
 
 }
