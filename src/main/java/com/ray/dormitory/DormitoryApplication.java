23450c8acea8e2fad1983e8f249ddff733994e5f
zengrc <1756332466@qq.com>
2023-01-02 19:30:15 +0800

    删除多余的@EnableTransactionManagement

--- src/main/java/com/ray/dormitory/DormitoryApplication.java
+++ src/main/java/com/ray/dormitory/DormitoryApplication.java
@@ -3,13 +3,11 @@ package com.ray.dormitory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Ray
  */
 @SpringBootApplication
-@EnableTransactionManagement
 @MapperScan("com.ray.dormitory.mapper")
 public class DormitoryApplication {
 
